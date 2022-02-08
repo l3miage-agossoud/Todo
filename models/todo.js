37a8eac3e6c0bcc47a20767ab32486be5e3d3330
l3miage-agossoud <romeo.agossou50@gmail.com>
2022-02-08 10:10:03 +0100

    new

--- models/todo.js
+++ models/todo.js
@@ -3,16 +3,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
-  title : String,
-  description : String,
-  done : {
-    type : Boolean,
-    default : false
-  },
-  createdAt : {
-    type : Date,
-    default : Date.now
-  },
+    title: String,
+    description: String,
+    done: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
--- routes/todo.js
+++ routes/todo.js
@@ -4,40 +4,40 @@ const router = require('express').Router();
 //CREATE
 router.post("/todo", (req, res) => {
     let Todo = require('../models/todo');
-    let newTodo = Todo ({
+    let newTodo = Todo({
         title: req.body.title,
-        description : req.body.description
+        description: req.body.description
     });
-  
-    newTodo.save()
-    .then((savedTodo) => {
 
-        //send back the created Todo
-        res.json(savedTodo);
-            
-    }, (err) => {
-        res.status(400).json(err)
-    });
+    newTodo.save()
+        .then((savedTodo) => {
+
+            //send back the created Todo
+            res.json(savedTodo);
+
+        }, (err) => {
+            res.status(400).json(err)
+        });
 
 });
 
 //READ
 router.get("/todos", (req, res) => {
-    res.json({"msg": "Read all the todos"});
+    res.json({ "msg": "Read all the todos" });
 });
 
 router.get("/todo", (req, res) => {
-    res.json({"msg": "Read a todo"});
+    res.json({ "msg": "Read a todo" });
 });
 
 //UPDATE
 router.put("/todo", (req, res) => {
-    res.json({"msg": "Update a Todo"});
+    res.json({ "msg": "Update a Todo" });
 });
 
 //DELETE
 router.delete("/todo", (req, res) => {
-    res.json({"msg": "Delete a Todo"});
+    res.json({ "msg": "Delete a Todo" });
 });
 
 module.exports = router;
